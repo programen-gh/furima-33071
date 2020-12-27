a5956740ad2c43909c6cf03a6c320dd568b36354
programen-gh <takayuki.tanazawa1026@gmail.com>
2020-12-27 11:53:23 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname,            length: { maximum: 40 }
+    validates :first_name,          format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
+    validates :last_name,           format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' }
+    validates :first_name_katakana, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' }
+    validates :last_name_katakana,  format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' }
+    validates :birthday
+  end
+end
