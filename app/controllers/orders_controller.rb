04c26404d9b2f0df1fd82ab1fea3bd5d602cbcc5
programen-gh <takayuki.tanazawa1026@gmail.com>
2021-01-07 13:36:42 +0900

    rubocop実行

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   before_action :move_to_index, only: :index
 
   def index
-    @order_address = OrderAddress.new()
+    @order_address = OrderAddress.new
     @item = Item.find(params[:item_id])
   end
 
@@ -22,11 +22,13 @@ class OrdersController < ApplicationController
   private
 
   def order_address_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def pay_item
-    Payjp::api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
       card: order_address_params[:token],
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -11,71 +11,71 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address).to be_valid
       end
       it 'building_name以外が正しく入力されている' do
-        @order_address.building_name = ""
+        @order_address.building_name = ''
         expect(@order_address).to be_valid
       end
     end
 
     context '購入ができないとき' do
       it 'tokenが空' do
-        @order_address.token = ""
+        @order_address.token = ''
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Token can't be blank")
       end
       it 'postal_codeが空' do
-        @order_address.postal_code = ""
+        @order_address.postal_code = ''
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
       end
       it 'postal_codeにハイフンが存在しない' do
-        @order_address.postal_code = "1234567"
+        @order_address.postal_code = '1234567'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code Input correctly")
+        expect(@order_address.errors.full_messages).to include('Postal code Input correctly')
       end
       it 'postal_codeのハイフンの位置が正しくない' do
-        @order_address.postal_code = "1234-567"
+        @order_address.postal_code = '1234-567'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code Input correctly")
+        expect(@order_address.errors.full_messages).to include('Postal code Input correctly')
       end
       it 'postal_codeが全角数字' do
-        @order_address.postal_code = "１２３-４５６７"
+        @order_address.postal_code = '１２３-４５６７'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Postal code Input correctly")
+        expect(@order_address.errors.full_messages).to include('Postal code Input correctly')
       end
       it 'prefecture_idが空（1を選択）' do
         @order_address.prefecture_id = 1
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Prefecture Select")
+        expect(@order_address.errors.full_messages).to include('Prefecture Select')
       end
       it 'municipalityが空' do
-        @order_address.municipality = ""
+        @order_address.municipality = ''
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Municipality can't be blank")
       end
       it 'house_numberが空' do
-        @order_address.house_number = ""
+        @order_address.house_number = ''
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("House number can't be blank")
       end
       it 'phone_numberが空' do
-        @order_address.phone_number = ""
+        @order_address.phone_number = ''
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
       end
       it 'phone_numberが12桁以上' do
-        @order_address.phone_number = "000000000000"
+        @order_address.phone_number = '000000000000'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number Input correctly")
+        expect(@order_address.errors.full_messages).to include('Phone number Input correctly')
       end
       it 'phone_numberにハイフンが存在する' do
-        @order_address.phone_number = "090-1234567"
+        @order_address.phone_number = '090-1234567'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number Input correctly")
+        expect(@order_address.errors.full_messages).to include('Phone number Input correctly')
       end
       it 'phone_numberが全角数字' do
-        @order_address.phone_number = "０００００００００００"
+        @order_address.phone_number = '０００００００００００'
         @order_address.valid?
-        expect(@order_address.errors.full_messages).to include("Phone number Input correctly")
+        expect(@order_address.errors.full_messages).to include('Phone number Input correctly')
       end
     end
   end
