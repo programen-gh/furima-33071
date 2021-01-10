0d71eebd6a8589e4f7394a864858be83d6d2fc07
programen-gh <takayuki.tanazawa1026@gmail.com>
2021-01-10 10:26:54 +0900

    金額表示のアルゴリズム変更

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -2,12 +2,11 @@ function calcPrice() {
   const price = document.getElementById('item-price')
   const tax = document.getElementById('add-tax-price')
   const profit = document.getElementById('profit')
-  
-  if(price != null){
+    
+  price.addEventListener('input', () => {
     tax.innerHTML = Math.floor(price.value * 0.1)
     profit.innerHTML = price.value - tax.innerHTML
-  }
+  })
 }
 
-setInterval(calcPrice, 100)
-
+window.addEventListener('load', calcPrice)
