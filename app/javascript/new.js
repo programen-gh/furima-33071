5bd76b5556517a2aa6546bdafab596e6ecbac416
programen-gh <takayuki.tanazawa1026@gmail.com>
2021-01-10 14:43:30 +0900

    JavaScript実行条件の分岐

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,12 +1,14 @@
-function calcPrice() {
-  const price = document.getElementById('item-price')
-  const tax = document.getElementById('add-tax-price')
-  const profit = document.getElementById('profit')
-    
-  price.addEventListener('input', () => {
-    tax.innerHTML = Math.floor(price.value * 0.1)
-    profit.innerHTML = price.value - tax.innerHTML
-  })
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )){
+  function calcPrice() {
+    const price = document.getElementById('item-price')
+    const tax = document.getElementById('add-tax-price')
+    const profit = document.getElementById('profit')
+      
+    price.addEventListener('input', () => {
+      tax.innerHTML = Math.floor(price.value * 0.1)
+      profit.innerHTML = price.value - tax.innerHTML
+    })
+  }
+  
+  window.addEventListener('load', calcPrice)
 }
-
-window.addEventListener('load', calcPrice)
